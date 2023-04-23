a44dedbd26dad7f43b829a34d5d1be55a5f986ac
Dima Grossman <dima@grossman.io>
2023-04-23 20:41:32 +0300

    fix: test cached packages folder

--- scripts/get-packages-folder.mjs
+++ scripts/get-packages-folder.mjs
@@ -1,6 +1,16 @@
 import { readProjects } from '@pnpm/filter-workspace-packages';
+import * as fs from 'fs';
 
 export async function getPackageFolders(folder) {
+  const cachePath = '/' + folder + '-changes-cache.json';
+
+  const isCacheExists = fs.existsSync(cachePath);
+  if (isCacheExists) {
+    const cache = fs.readFileSync(cachePath, 'utf8');
+
+    return JSON.parse(cache);
+  }
+
   const path = process.cwd();
   const content = await readProjects(path, [{
     parentDir: folder,
@@ -13,6 +23,9 @@ export async function getPackageFolders(folder) {
 
     return contains;
   } ).map((project) => project.manifest.name);
+
+  // store results in cache file for later use
+  fs.writeFileSync(cachePath, JSON.stringify(filteredItems));
 
   return filteredItems;
 }
